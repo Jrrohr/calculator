ccc840f0a3e0df65403dd32c987d80e82f9efaed
Jrrohr <jbanzet17@gmail.com>
2022-11-27 19:07:20 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var displayDiv = document.querySelector("#display");
+var count = "";
+var total = 0;
+
+function press(num){
+    count += num;
+    displayDiv.innerText = count;
+    return count;
+}
+
+function setOP(op){
+    count += op;
+    return count;
+}
+
+function calculate(){
+    count = eval(count);
+    displayDiv.innerText = count;
+    return count;
+}
+
+function clr(){
+    displayDiv.innerText = 0;
+    count = 0;
+    return count;
+}
